19adbd45746529d7d192e9fdc6daa3ac0c5479b8
Nguyen Tan Khoi <tankhoi46@gmail.com>
2024-05-26 09:57:31 +0700

    fix: add missing contructors, getters, setters in class User

--- backend/src/main/java/com/backend/exclusive/models/User.java
+++ backend/src/main/java/com/backend/exclusive/models/User.java
@@ -1,8 +1,8 @@
 package com.backend.exclusive.models;
 
-import lombok.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
@@ -11,15 +11,149 @@ import java.util.Date;
 @NoArgsConstructor
 @Document(collection = "users")
 public class User {
-     private ObjectId id;
-     private String email;
-     private String password;
-     private String username;
-     private String firstName;
-     private String lastName;
-     private String phoneNumber;
-     private String address;
-     private boolean isDeleted;
-     private Date createdAt;
-     private Date updatedAt;
+    private ObjectId id;
+    private String email;
+    private String password;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String address;
+    private boolean isDeleted;
+    private Date createdAt;
+    private Date updatedAt;
+
+    public User(String email,
+                String password,
+                String username,
+                String firstName,
+                String lastName,
+                String phoneNumber,
+                String address,
+                boolean isDeleted,
+                Date createdAt,
+                Date updatedAt) {
+        this.email = email;
+        this.password = password;
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.isDeleted = isDeleted;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public User(ObjectId id,
+                String email,
+                String password,
+                String username,
+                String firstName,
+                String lastName,
+                String phoneNumber,
+                String address,
+                boolean isDeleted,
+                Date createdAt,
+                Date updatedAt) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.isDeleted = isDeleted;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public boolean isDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        isDeleted = deleted;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
