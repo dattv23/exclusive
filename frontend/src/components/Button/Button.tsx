0b898aa42fbac55af5797785f5a5c76fffe8b282
dattv23 <datvan635@gmail.com>
2024-06-01 22:18:33 +0700

    feat: button component

---
+++ frontend/src/components/Button/Button.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { cva, VariantProps } from 'class-variance-authority';
+
+import { cn } from '@/utils';
+
+const buttonVariants = cva(
+  'inline-flex items-center justify-center rounded text-base font-medium hover:opacity-80',
+  {
+    variants: {
+      variant: {
+        primary: 'bg-[#DB4444] text-white',
+        secondary: 'bg-white border border-[#cccc] text-black',
+      },
+      size: {
+        default: 'px-12 py-4',
+        sm: 'px-12 py-3',
+      },
+      state: {
+        default: 'opacity-100',
+        disabled: 'opacity-15',
+      },
+    },
+    defaultVariants: {
+      variant: 'primary',
+      size: 'default',
+      state: 'default',
+    },
+  },
+);
+
+interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {}
+
+const Button: React.FC<ButtonProps> = ({
+  variant,
+  size,
+  className,
+  children,
+  ...props
+}) => {
+  return (
+    <button
+      className={cn(buttonVariants({ variant, size, className }))}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
--- frontend/src/components/index.tsx
+++ frontend/src/components/index.tsx
@@ -7,3 +7,4 @@ export { default as WishList } from './WishList';
 export { default as LocaleSwitcher } from './LocaleSwitcher';
 export { default as LocaleSwitcherSelect } from './LocaleSwitcherSelect';
 export { default as AccountDropdown } from './AccountDropdown';
+export { default as Button } from './Button';
