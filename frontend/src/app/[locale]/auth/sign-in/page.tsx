f1bd945232f5b176c6e4bd9e38a599ef19fbaa36
dattv23 <datvan635@gmail.com>
2024-06-11 00:44:55 +0700

    fix: request locale of sign in, sign up

--- frontend/src/app/[locale]/auth/sign-in/page.tsx
+++ frontend/src/app/[locale]/auth/sign-in/page.tsx
@@ -1,13 +1,22 @@
 import dynamic from 'next/dynamic';
 import React from 'react';
+import { unstable_setRequestLocale } from 'next-intl/server';
 
 import LoadingPage from '@/containers/loading-page';
+import { Locale } from '@/config';
 
 const SignInPage = dynamic(() => import('@/containers/auth-page/signin-page'), {
   loading: () => <LoadingPage />,
 });
 
-const SignIn: React.FC = () => {
+type SignInProps = {
+  params: {
+    locale: Locale;
+  };
+};
+
+const SignIn: React.FC<SignInProps> = ({ params }) => {
+  unstable_setRequestLocale(params.locale);
   return <SignInPage />;
 };
 
--- frontend/src/app/[locale]/auth/sign-up/page.tsx
+++ frontend/src/app/[locale]/auth/sign-up/page.tsx
@@ -2,12 +2,21 @@ import dynamic from 'next/dynamic';
 import React from 'react';
 
 import LoadingPage from '@/containers/loading-page';
+import { Locale } from '@/config';
+import { unstable_setRequestLocale } from 'next-intl/server';
 
 const SignUpPage = dynamic(() => import('@/containers/auth-page/signup-page'), {
   loading: () => <LoadingPage />,
 });
 
-const SignUp: React.FC = () => {
+type SignUpProps = {
+  params: {
+    locale: Locale;
+  };
+};
+
+const SignUp: React.FC<SignUpProps> = ({ params }) => {
+  unstable_setRequestLocale(params.locale);
   return <SignUpPage />;
 };
 
