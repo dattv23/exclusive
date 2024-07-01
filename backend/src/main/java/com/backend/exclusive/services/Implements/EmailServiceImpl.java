0c63f8fb0294d42ebb2965e22fc15f3bc6f86ec5
tuantran <146686124+Tuna1423@users.noreply.github.com>
2024-07-01 14:44:08 +0700

    refactor: style email

--- backend/src/main/java/com/backend/exclusive/services/Implements/EmailServiceImpl.java
+++ backend/src/main/java/com/backend/exclusive/services/Implements/EmailServiceImpl.java
@@ -24,23 +24,31 @@ public class EmailServiceImpl implements EmailService {
             helper.setTo(toEmail);
             helper.setSubject("Welcome to Our Service");
 
-            String html = "<div style=\"font-family: Helvetica, Arial, sans-serif; min-width: 1000px; overflow: auto; line-height: 2\">\n" +
-                    "\t<div style=\"margin: 50px auto; width: 70%; padding: 20px 0\">\n" +
-                    "\t\t<div style=\"border-bottom: 1px solid #eee\"><a href=\"\" style=\"font-size: 1.4em; color: #00466a; text-decoration: none; font-weight: 600\">Exclusive Welcome</a></div>\n" +
-                    "\t\t<p style=\"font-size: 1.1em\">Hi, {toEmail}</p>\n" +
-                    "\t\t<p>Thank you for signing up. We're glad to have you with us!</p>\n" +
-                    "\t\t<img src=\"https://images.pexels.com/photos/3643925/pexels-photo-3643925.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1\" width=\"480\" height=\"auto\" style=\"\" frameBorder=\"0\" class=\"giphy-embed\" allowFullScreen></img>\n" +
-                    "    <p style=\"font-size: 0.9em\">Regards,<br />Exclusive</p>\n" +
-                    "\t\t<hr style=\"border: none; border-top: 1px solid #eee\" />\n" +
-                    "\t\t<div style=\"float: right; padding: 8px 0; color: #aaa; font-size: 0.8em; line-height: 1; font-weight: 300\">\n" +
-                    "\t\t\t<p>Exclusive Inc</p>\n" +
-                    "\t\t\t<p>Thu Duc, Ho Chi Minh</p>\n" +
-                    "\t\t\t<p>Viet Nam</p>\n" +
-                    "\t\t</div>\n" +
-                    "\t</div>\n" +
-                    "</div>";
+            StringBuilder html = new StringBuilder();
+            html.append("<div style=\"font-family: Helvetica, Arial, sans-serif; min-width: 1000px; overflow: auto; line-height: 2; text-align: center;\">\n");
+            html.append("\t<div style=\"margin: 50px auto; width: 70%; padding: 20px 0;\">\n");
+            html.append("\t\t<div style=\"border-bottom: 1px solid #eee; text-align: center;\">\n");
+            html.append("\t\t\t<a href=\"\" style=\"font-size: 1.4em; color: #00466a; text-decoration: none; font-weight: 600;\">Exclusive Welcome</a>\n");
+            html.append("\t\t</div>\n");
+            html.append("\t\t<p style=\"font-size: 1.1em; text-align: center; color: black;\">Hi, {toEmail}</p>\n");
+            html.append("\t\t<p style=\"color: black;\">Thank you for signing up. We're glad to have you with us!</p>\n");
+            html.append("\t\t<p style=\"color: black;\">Your account has been successfully created. Here are your details:</p>\n");
+            html.append("\t\t<ul style=\"font-size: 1em; line-height: 1.5; list-style-type: none; padding: 0; text-align: left; display: inline-block; color: black;\">\n");
+            html.append("\t\t\t<li><strong>Email:</strong> {toEmail}</li>\n");
+            html.append("\t\t</ul>\n");
+            html.append("\t\t<p style=\"color: black;\">To log in you need to complete the following procedures: <a href=\"#\" style=\"color: #00466a; text-decoration: none; font-weight: 600;\">Click here</a>.</p>\n");
+            html.append("\t\t<img src=\"https://images.pexels.com/photos/3643925/pexels-photo-3643925.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1\" width=\"480\" height=\"auto\" style=\"\" frameBorder=\"0\" class=\"giphy-embed\" allowFullScreen></img>\n");
+            html.append("\t\t<p style=\"font-size: 0.9em; color: black;\">Regards,<br />Exclusive Team</p>\n");
+            html.append("\t\t<hr style=\"border: none; border-top: 1px solid #eee\" />\n");
+            html.append("\t\t<div style=\"float: right; padding: 8px 0; color: #aaa; font-size: 0.8em; line-height: 1; font-weight: 300; text-align: center;\">\n");
+            html.append("\t\t\t<p>Exclusive Inc</p>\n");
+            html.append("\t\t\t<p>Thu Duc, Ho Chi Minh</p>\n");
+            html.append("\t\t\t<p>Viet Nam</p>\n");
+            html.append("\t\t</div>\n");
+            html.append("\t</div>\n");
+            html.append("</div>");
 
-            String htmlContent = html.replace("{toEmail}", toEmail);
+            String htmlContent = html.toString().replace("{toEmail}", toEmail);
             helper.setText(htmlContent, true);  // `true` indicates HTML content
 
             mailSender.send(mimeMessage);
