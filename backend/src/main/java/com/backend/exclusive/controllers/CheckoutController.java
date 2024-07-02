9a673770410ee78f9da336f32b8f53d0f56a4436
Nguyen Tan Khoi <tankhoi46@gmail.com>
2024-07-02 16:04:45 +0700

    feat, fix: get order pay status, checkout bug

--- backend/src/main/java/com/backend/exclusive/controllers/CheckoutController.java
+++ backend/src/main/java/com/backend/exclusive/controllers/CheckoutController.java
@@ -3,15 +3,22 @@ package com.backend.exclusive.controllers;
 import com.backend.exclusive.common.ApiResponse;
 import com.backend.exclusive.common.ResponseUtil;
 import com.backend.exclusive.dtos.OrderDTO;
+import com.backend.exclusive.dtos.OrderItemDTO;
 import com.backend.exclusive.mappers.OrderMapper;
 import com.backend.exclusive.models.Order;
+import com.backend.exclusive.models.User;
 import com.backend.exclusive.repositories.UserRepository;
 import com.backend.exclusive.services.CheckoutService;
 import com.backend.exclusive.services.EmailService;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/checkout")
@@ -31,13 +38,26 @@ public class CheckoutController {
 
     @PostMapping
     public ResponseEntity<ApiResponse<OrderDTO>> checkout(@RequestBody OrderDTO orderDTO, @RequestParam String paymentMethodId) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User currentUser = (User) authentication.getPrincipal();
+
+        // init data order
+        orderDTO.setUserId(currentUser.getId().toString());
+        orderDTO.setOrderDate(new Date());
+
+        List<OrderItemDTO> orderItemDTOS = orderDTO.getOrderItems();
+        double total = orderItemDTOS.stream()
+                .mapToDouble(OrderItemDTO::getSubtotal)
+                .sum();
+        orderDTO.setTotalAmount(total);
+
+        orderDTO.setStatus("Pending");
         Order newOrder = checkoutService.processOrder(orderDTO, paymentMethodId);
 
         // Send order placed email
         String toEmail = userRepository.findById(new ObjectId(orderDTO.getUserId())).get().getEmail();
         emailService.sendOrderPlacedEmail(toEmail);
 
-        System.out.printf(toEmail);
         return ResponseUtil.success(orderMapper.toOrderDTO(newOrder));
     }
 }
