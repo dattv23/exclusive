090cce7c46751b6ba0061cf486942e24b19ae64e
Nguyen Tan Khoi <tankhoi46@gmail.com>
2024-07-03 12:10:14 +0700

    refactor: crud product

--- backend/src/main/java/com/backend/exclusive/controllers/admin/AdminController.java
+++ backend/src/main/java/com/backend/exclusive/controllers/admin/AdminController.java
@@ -2,7 +2,10 @@ package com.backend.exclusive.controllers.admin;
 
 import com.backend.exclusive.dtos.ProductDTO;
 import com.backend.exclusive.mappers.ProductMapper;
+import com.backend.exclusive.models.Product;
 import com.backend.exclusive.repositories.ProductRepository;
+import com.backend.exclusive.services.CategoryService;
+import com.backend.exclusive.services.CloudinaryService;
 import com.backend.exclusive.services.ProductService;
 import jakarta.validation.Valid;
 import org.bson.types.ObjectId;
@@ -11,6 +14,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Controller
 @RequestMapping("/api/v1/admin")
@@ -18,6 +24,12 @@ public class AdminController {
 
     @Autowired
     private ProductService productService;
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
+    private CloudinaryService cloudinaryService;
 
     @Autowired
     private ProductMapper productMapper;
@@ -37,47 +49,76 @@ public class AdminController {
     }
 
     //    product part
+    // list
     @GetMapping("/product")
     public String listProducts(Model model) {
         model.addAttribute("products", productService.getAll());
         return "admin/product/index";
     }
 
+    // create
     @GetMapping("/products/new")
     public String showCreateProductForm(Model model) {
         model.addAttribute("product", new ProductDTO());
+        model.addAttribute("categories", categoryService.getAllCategories());
         return "admin/product/create";
     }
 
     @PostMapping("/products")
-    public String createProduct(@ModelAttribute("product") @Valid ProductDTO productDTO, BindingResult result, Model model) {
+    public String createProduct(@ModelAttribute("product") @Valid ProductDTO productDTO,
+                                BindingResult result, Model model,
+                                @RequestParam("imageFile") MultipartFile image) throws IOException {
+        System.out.println(result);
         if (result.hasErrors()) {
             return "admin/product/create";
         }
-        productRepository.save(productMapper.toProduct(productDTO));
-        return "redirect:/admin/products";
+        productDTO.setImageUrl(cloudinaryService.uploadImage(image));
+        productDTO.setCategoryName(categoryService.getCategoryById(new ObjectId(productDTO.getCategoryId())).get().getName());
+        productService.create(productDTO, productDTO.getImageUrl());
+        return "redirect:/api/v1/admin/product";
     }
 
+    // edit
     @GetMapping("/products/edit/{id}")
     public String showUpdateProductForm(@PathVariable("id") String id, Model model) {
-        ProductDTO productDTO = productMapper.toProductDTO(productService.getById(new ObjectId(id)).get());
+        Product product = productService.getById(new ObjectId(id)).get();
+        ProductDTO productDTO = productMapper.toProductDTO(product);
+        productDTO.setRate(product.getRate());
+        productDTO.setNumberOfRate(product.getNumberOfRate());
+        productDTO.setStatus(product.getStatus());
         model.addAttribute("product", productDTO);
+        model.addAttribute("categories", categoryService.getAllCategories());
         return "admin/product/edit";
     }
 
     @PostMapping("/products/edit/{id}")
-    public String updateProduct(@PathVariable("id") String id, @ModelAttribute("product") @Valid ProductDTO productDTO, BindingResult result, Model model) {
+    public String updateProduct(@PathVariable("id") String id,
+                                @ModelAttribute("product") @Valid ProductDTO productDTO,
+                                BindingResult result,
+                                Model model,
+                                @RequestParam("imageFile") MultipartFile image) throws IOException {
+        System.out.println(result);
+        System.out.println(productDTO);
         if (result.hasErrors()) {
             return "admin/product/edit";
         }
-        productService.update(new ObjectId(id), productDTO);
-        return "redirect:/admin/products";
+
+        if (!image.isEmpty()) {
+            productDTO.setImageUrl(cloudinaryService.uploadImage(image));
+            productDTO.setCategoryName(categoryService.getCategoryById(new ObjectId(productDTO.getCategoryId())).get().getName());
+            productService.update(new ObjectId(id), productDTO, productDTO.getImageUrl());
+        } else {
+            productDTO.setCategoryName(categoryService.getCategoryById(new ObjectId(productDTO.getCategoryId())).get().getName());
+            productService.update(new ObjectId(id), productDTO);
+        }
+        return "redirect:/api/v1/admin/product";
     }
 
+    // delete
     @GetMapping("/products/delete/{id}")
     public String deleteProduct(@PathVariable("id") String id) {
         productService.delete(new ObjectId(id));
-        return "redirect:/admin/products";
+        return "redirect:/api/v1/admin/product";
     }
 
     //    category part
